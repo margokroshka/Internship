e9d77bc09140e777ebd85355bbacb09c32152237
margaret <m.i.baranovskaya13@gmail.com>
2024-03-27 20:24:31 +0300

    Initial commit

---
+++ tack1/src/main/java/baranovskaya/FileSystem.java
@@ -0,0 +1,28 @@
+package baranovskaya;
+import java.util.Scanner;
+
+
+public class FileSystem {
+    private Folder currentFolder = new Folder("root");
+
+    public void add(String element){
+        currentFolder = PathBuilder.processInput(element, currentFolder);
+    }
+    public String print (){
+        return  currentFolder.toString();
+    }
+
+    public static void main(String[] args){
+        FileSystem fs = new FileSystem();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("exit")) {
+                break;
+            }else {
+                fs.add(input);
+                System.out.println(fs.print());
+            }
+        }
+    }
+}
